6789eabb55817190393ead7bb8cc62a8ae2d0c64
wangshuyuan.h <wangshuyuan.h@bytedance.com>
2021-08-08 21:33:22 +0800

    feat: add transpiling

---
+++ demo/1_transpiling/index.ts
@@ -0,0 +1,7 @@
+import * as ts from "typescript";
+
+const source = "let x: string  = 'string'";
+
+let result = ts.transpileModule(source, { compilerOptions: { target: ts.ScriptTarget.ESNext }});
+
+console.log(JSON.stringify(result));
